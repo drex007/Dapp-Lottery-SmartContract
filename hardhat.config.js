a30fe44c32ae3025aef188f848d90e7fc33fd0ed
drex007 <vykeoj@gmail.com>
2023-06-12 09:54:33 +0000

    chainlink vrf corrections

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,9 +6,36 @@ require("hardhat-gas-reporter")
 require("hardhat-contract-sizer")
 require("dotenv").config()
 
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || " "
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const COINMARKETCAP__API_KEY = process.env.COINMARKETCAP__API_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      blockConfirmations: 1,
+    },
+    sepolia: {
+      chainId: 11155111,
+      blockConfirmations: 6,
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY]
+
+    }
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0
+    },
+    player: {
+      default: 1,
+    }
+  },
   solidity: {
     compilers: [
       {
@@ -19,4 +46,5 @@ module.exports = {
       },
     ],
   },
+
 }; 
